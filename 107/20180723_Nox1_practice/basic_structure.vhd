4a6c2e231908e7590fb48e47a97a6635cef7f019
a34007024 <a34007024@gmail.com>
2018-07-23 15:18:28 +0800

    add basic_structure file
    新增檔案

---
+++ 107/20180723_Nox1_practice/basic_structure.vhd
@@ -0,0 +1,30 @@
+Library IEEE;
+Use IEEE.std_logic_1164.all;
+Use IEEE.std_logic_unsigned.all;
+----------------------------------------
+entity basic_structure is
+	port(
+	
+	);
+end basic_structure;
+-----------------------------------------
+architecture test1 of basic_structure is
+	
+begin
+	process()
+	
+	begin
+	
+	end process;
+end test1;
+
+
+
+
+
+
+
+
+
+
+
