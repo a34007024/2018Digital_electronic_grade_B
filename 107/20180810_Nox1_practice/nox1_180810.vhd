8da3a3c0993e64c059e001d7e994cddcc229f1c6
a34007024 <a34007024@gmail.com>
2018-08-10 10:13:29 +0800

    180810 nox1 create basic file

---
+++ 107/20180810_Nox1_practice/nox1_180810.vhd
@@ -0,0 +1,14 @@
+Library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logoc_unsigned.all;
+
+entity nox1_180810 is
+	port();
+	
+end entity;
+
+architecture main of nox1_180810 is
+
+begin
+
+end architecture;
